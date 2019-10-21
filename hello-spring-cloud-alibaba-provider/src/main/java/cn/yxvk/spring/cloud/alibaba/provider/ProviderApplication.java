cc475c63d67fb4a315e29fc28a03722161bf2221
yxArcadia <44748466+yxArcadia@users.noreply.github.com>
2019-10-21 23:48:26 +0800

    init

---
+++ hello-spring-cloud-alibaba-provider/src/main/java/cn/yxvk/spring/cloud/alibaba/provider/ProviderApplication.java
@@ -0,0 +1,11 @@
+package cn.yxvk.spring.cloud.alibaba.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class,args);
+    }
+}
